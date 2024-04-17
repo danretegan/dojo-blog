71564a8f3ebe6ab708e29a369be2900d7483b53c
Dan Retegan <danretegan@yahoo.com>
2024-04-17 17:33:06 +0300

    split constants

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,9 @@
 import BlogList from 'BlogList';
 import { useState } from 'react';
+import { blogs as blogData } from './constants';
 
 const Home = () => {
-  const [blogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs] = useState(blogData);
 
   return (
     <div className="home">

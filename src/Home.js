0e09ec53fec22bbeece604e917b9428afd285559
Dan Retegan <danretegan@yahoo.com>
2024-04-17 18:38:46 +0300

    #15 - useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,8 @@ import { blogs as blogData } from './constants';
 const Home = () => {
   const [blogs, setBlogs] = useState(blogData);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = id => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -12,12 +14,16 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran!');
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs:" handleDelete={handleDelete} />
+
+      <button onClick={() => setName('luigi')}>Change name</button>
+
+      <p>{name}</p>
     </div>
   );
 };

adddff9d8f32c3992ea98d8ca19339fe488dbade
Dan Retegan <danretegan@yahoo.com>
2024-04-17 17:18:46 +0300

    #12 - Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,15 @@ const Home = () => {
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs:" />
+      <BlogList blogs={blogs} title="All blogs:" />
+      <BlogList
+        blogs={blogs.filter(blog => blog.author === 'mario')}
+        title="Mario's blogs:"
+      />
+      <BlogList
+        blogs={blogs.filter(blog => blog.author === 'yoshi')}
+        title="Yoshi's blogs:"
+      />
     </div>
   );
 };

df1e76d2af9c1fed17bf8ef306276f4b3ff52d1a
Dan Retegan <danretegan@yahoo.com>
2024-04-18 14:49:05 +0300

    #25 - Route Parameters

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import BlogDetails from 'BlogDetails';
 import Create from './Create';
 import Home from './Home';
 import Navbar from './Navbar';
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div>
       </div>

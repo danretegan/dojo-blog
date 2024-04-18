6b650db97c943a7aacee45ff391ff8b387586ac5
Dan Retegan <danretegan@yahoo.com>
2024-04-18 19:18:32 +0300

    #27 - Controlled Inputs (forms)

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
   return (
     <div className="create">
       <h2>Add a new blog:</h2>
+
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={evt => setTitle(evt.target.value)}
+        />
+
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={evt => setBody(evt.target.value)}
+        ></textarea>
+
+        <label>Blog author:</label>
+        <select value={author} onChange={evt => setAuthor(evt.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+
+        <button>Add blog!</button>
+
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };

dc3e772a87564be133169f5ed24735516a2fe8b7
Dan Retegan <danretegan@yahoo.com>
2024-04-19 07:40:34 +0300

    Autostart server setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/dojo-blog">
       <div className="App">
         <Navbar />
         <div className="content">
